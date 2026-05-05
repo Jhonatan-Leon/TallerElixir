690acc47e3acbacdf18a5af1e013206d4904d042
Jhonatan-Leon <c.jhonatan2007@gmail.com>
2026-05-05 15:20:25 -0500

    feat: Se añadio funcionalidades para actualización y busqueda de socios
    feat: Implementación de una validación
    
    Implmentación de intento de controlar errores
    
    feat: Implementación de solicitud de datos

--- gimnasio/lib/App/helpers/solicitud_datos.ex
+++ gimnasio/lib/App/helpers/solicitud_datos.ex
@@ -13,4 +13,33 @@ defmodule Gimnasio.Solicitud do
     end
   end
 
+
+  def solicitar_actualizacion() do
+    cedula = IO.gets("¿Que socio desea actualizar?,
+    por favor, ingrese su cédula: ") |> String.trim()
+
+    nombre = IO.gets("Ingrese el nuevo nombre del socio: ") |> String.trim()
+    edad = IO.gets("Ingrese la nueva edad del socio: ") |> String.trim()
+
+    case Gimnasio.Validacion.validacion_datos({cedula, nombre, edad}) do
+      {:ok, {cedula, nombre, edad}} ->
+        {cedula, nombre, edad}
+      {:error, razon} ->
+        IO.inspect(razon, label: "Error: ")
+        solicitar_actualizacion()
+    end
+
+  end
+
+  def solicitar_cedula() do
+    cedula = IO.gets("Ingrese la cédula del socio que desea buscar: ") |> String.trim()
+
+    case Gimnasio.Validacion.validacion_cedula(cedula) do
+      {:ok, cedula} ->
+        {:ok, cedula}
+      {:error, razon} ->
+        IO.inspect(razon, label: "Hay un error al ingresar la cédula #{razon}")
+        solicitar_cedula()
+    end
+  end
 end
